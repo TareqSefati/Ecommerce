37d77cce50b17f88429e4d41900d93fe19a9cd39
TareqSefati <tareq.sefati@gmail.com>
2018-02-09 00:21:39 +0600

    failed to show data in console using ajax form() method

--- src/main/java/com/bjit/ecommerce/controller/RestUserController.java
+++ src/main/java/com/bjit/ecommerce/controller/RestUserController.java
@@ -4,11 +4,13 @@ import java.util.Date;
 
 import javax.validation.Valid;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bjit.ecommerce.entity.User;
@@ -16,18 +18,22 @@ import com.bjit.ecommerce.entity.User;
 @RestController
 public class RestUserController {
 
-    @PostMapping("/admin/userManager/getUsers")
+    @PostMapping(value="/admin/userManager/getUsers", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+             headers="charset=UTF-8,content-type=text/plain")
+    @ResponseBody
     public ResponseEntity<?> getAllRestUsers(@Valid @RequestBody User user, BindingResult result, Errors errors) {
     	
     	User user1 = new User(1, "aaaaaaa", "bbbbbb", "aaaa@ss.com", "std12345", "111111111", "active", "dhaka dhaka", "dhaka dhaka", 2, "1212", "dhaka dhaka", "dhaka dhaka", 2, "1212", new Date(), new Date());
+    	System.out.println(user);
     	user.setUserId(120);
     	user.setUserCreatedOn(new Date());
     	user.setUserUpdatedOn(new Date());
+    	System.out.println(user);
     	if(result.hasErrors()) {
     		System.out.println(result.getAllErrors());
     		return ResponseEntity.ok(errors);
     	} else {
-    		return ResponseEntity.ok(user);
+    		return ResponseEntity.ok(user1);
     	}
         
 
--- src/main/resources/static/javaScript/userManager.js
+++ src/main/resources/static/javaScript/userManager.js
@@ -4,12 +4,22 @@ $(document).ready(function () {
 
         //stop submit the form, we will post it manually.
         event.preventDefault();
-
+        var addUser = document.getElementById("addUser");
+        var fd = new FormData(addUser);
+        //console.log(fd);
+        var str = $("form").serialize();
+        console.log(str);
+        console.log(JSON.stringify(str));
+       // die();
     	$.ajax({
             type: "POST",
             contentType: "application/json",
             url: "/admin/userManager/getUsers",
-            
+            //data: JSON.stringify(str),
+            data: str,
+            processData: false,
+            contentType: false,
+           
             dataType: 'json',
             cache: false,
             timeout: 600000,
