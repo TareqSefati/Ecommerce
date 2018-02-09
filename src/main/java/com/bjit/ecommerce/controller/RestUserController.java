9e74511ec868bf4e799ee5e7e8dfad62e0d26878
Tareq Sefati <tareq.sefati@bjitgroup.com>
2018-02-09 17:47:18 +0600

    till unable to sort the previous problem

--- src/main/java/com/bjit/ecommerce/controller/RestUserController.java
+++ src/main/java/com/bjit/ecommerce/controller/RestUserController.java
@@ -1,7 +1,5 @@
 package com.bjit.ecommerce.controller;
 
-import java.util.Date;
-
 import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
@@ -10,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,24 +17,65 @@ import com.bjit.ecommerce.entity.User;
 @RestController
 public class RestUserController {
 
-    @PostMapping(value="/admin/userManager/getUsers", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-             headers="charset=UTF-8,content-type=text/plain")
-    @ResponseBody
-    public ResponseEntity<?> getAllRestUsers(@Valid @RequestBody User user, BindingResult result, Errors errors) {
-    	
-    	User user1 = new User(1, "aaaaaaa", "bbbbbb", "aaaa@ss.com", "std12345", "111111111", "active", "dhaka dhaka", "dhaka dhaka", 2, "1212", "dhaka dhaka", "dhaka dhaka", 2, "1212", new Date(), new Date());
-    	System.out.println(user);
-    	user.setUserId(120);
-    	user.setUserCreatedOn(new Date());
-    	user.setUserUpdatedOn(new Date());
-    	System.out.println(user);
-    	if(result.hasErrors()) {
-    		System.out.println(result.getAllErrors());
-    		return ResponseEntity.ok(errors);
-    	} else {
-    		return ResponseEntity.ok(user1);
-    	}
-        
-
-    }
+//    @PostMapping(value="/admin/userManager/getUsers", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+//             headers="charset=UTF-8,content-type=text/plain")
+//    @ResponseBody
+//    public String getAllRestUsers(HttpServletRequest request) {
+//    	
+//    	User user1 = new User(1, "aaaaaaa", "bbbbbb", "aaaa@ss.com", "std12345", "111111111", "active",
+//    			"dhaka dhaka", "dhaka dhaka", 2, "1212", "dhaka dhaka", "dhaka dhaka", 2, "1212", new Date(), new Date());
+////    	System.out.println(user);
+////    	user.setUserId(120);
+////    	user.setUserCreatedOn(new Date());
+////    	user.setUserUpdatedOn(new Date());
+////    	System.out.println(user);
+////    	if(result.hasErrors()) {
+////    		System.out.println(result.getAllErrors());
+////    		return ResponseEntity.ok(errors);
+////    	} else {
+////    		return ResponseEntity.ok(user);
+////    	}
+//    	return request.getParameter("firstName");
+//        
+//
+//    }
+	
+//	@GetMapping(value="/admin/userManager/getUsers")
+//	public String getAllRestUsers() {
+//		System.out.println("hi there.");
+//		return null;
+//	}
+	
+//	@PostMapping(value="/admin/userManager/getUsers")
+//	public ResponseEntity<User> getAllRestUsers(@Valid User user, BindingResult result) {
+//		System.out.println("hi there." + user);
+//		if(result.hasErrors()) {
+//			System.out.println("has error");
+//			return ResponseEntity.ok(null);
+//		}else {
+//			System.out.println("Found Data.");
+//			return ResponseEntity.ok(user);
+//		}
+//	}
+	
+	@PostMapping(value="/admin/userManager/getUsers", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+			headers="\"Content-Type\": \"application/json; charset=UTF-8\"")
+	@ResponseBody
+	public ResponseEntity<?> getAllRestUsers(@RequestBody  User user, BindingResult result, Errors errors) {
+		System.out.println("hi there.:  " + user);
+		if(result.hasErrors()) {
+			System.out.println("has error");
+			return ResponseEntity.ok(errors);
+		}else {
+			System.out.println("Found Data.");
+			return ResponseEntity.ok(user);
+			//return ResponseEntity.ok(user);
+		}
+		//return null;
+	}
+	
 }
+
+
+
+
--- src/main/resources/static/javaScript/userManager.js
+++ src/main/resources/static/javaScript/userManager.js
@@ -4,19 +4,47 @@ $(document).ready(function () {
 
         //stop submit the form, we will post it manually.
         event.preventDefault();
-        var addUser = document.getElementById("addUser");
-        var fd = new FormData(addUser);
+        
+//        var addUser = document.getElementById("addUser");
+//        var fd = new FormData(addUser);
         //console.log(fd);
-        var str = $("form").serialize();
-        console.log(str);
-        console.log(JSON.stringify(str));
+        
+//        var str = $("form").serializeArray();
+//        console.log(str);
+//        console.log(JSON.stringify(str));
+
+        var formData = {};
+        formData["userId"] = null;
+        formData["userFirstName"] = $("#firstName").val();
+        formData["userLastName"] = $("#lastName").val();
+        formData["userEmail"] = $("#email").val();
+        formData["userPassword"] = $("#password").val();
+        formData["userContactNumber"] = $("#contactNumber").val();
+        formData["userStatus"] = $("#userStatus").val();
+        
+        formData["userBillingAddress"] = $("#billingAddress").val();
+        formData["userBillingCity"] = $("#billingCity").val();
+        formData["userBillingZipcode"] = $("#billingZipcode").val();
+        formData["userBillingCountry"] = $("#billingCountry").val();
+        
+        formData["userShippingAddress"] = $("#shippingAddress").val();
+        formData["userShippingCity"] = $("#shippingCity").val();
+        formData["userShippingZipcode"] = $("#shippingZipcode").val();
+        formData["userShippingCountry"] = $("#shippingCountry").val();
+        
+        formData["userCreatedOn"] = null;
+        formData["userUpdatedOn"] = null;
+        
+        console.log(formData);
+        
+
        // die();
     	$.ajax({
             type: "POST",
             contentType: "application/json",
             url: "/admin/userManager/getUsers",
-            //data: JSON.stringify(str),
-            data: str,
+            //data: JSON.stringify(testData),
+            data: formData,
             processData: false,
             contentType: false,
            
